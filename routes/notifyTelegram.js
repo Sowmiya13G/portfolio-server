db32538de54979dcaaf033153aca92deb14e067c
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2025-06-19 21:40:54 +0530

    enc fixes

--- routes/notifyTelegram.js
+++ routes/notifyTelegram.js
@@ -7,25 +7,21 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   const { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, BASE_TELEGRAM_URL } =
     process.env;
-  const { country_name, city, region, ip, latitude, longitude, timestamp } =
-    req.body;
+  const { payload } = req.body;
 
-  const decryptedCountry = decryptText(country_name);
-  const decryptedCity = decryptText(city);
-  const decryptedRegion = decryptText(region);
-  const decryptedIP = decryptText(ip);
-  const decryptedLatitude = decryptText(latitude);
-  const decryptedLongitude = decryptText(longitude);
-  const decryptedTimestamp = decryptText(timestamp);
+  const decryptedJson = decryptText(payload);
+
+  const { country_name, city, region, ip, latitude, longitude, timestamp } =
+    JSON.parse(decryptedJson);
 
   const message = `
 👀 You've Got a Visitor!
 
-<b>Country:</b> ${decryptedCountry}
-<b>Location:</b> ${decryptedCity}, ${decryptedRegion}
-<b>IP Address:</b> ${decryptedIP}
-<b>Time:</b> ${decryptedTimestamp}
-<b>Map:</b> https://www.google.com/maps?q=${decryptedLatitude},${decryptedLongitude}
+<b>Country:</b> ${country_name}
+<b>Location:</b> ${city}, ${region}
+<b>IP Address:</b> ${ip}
+<b>Time:</b> ${timestamp}
+<b>Map:</b> https://www.google.com/maps?q=${latitude},${longitude}
   `;
 
   try {
